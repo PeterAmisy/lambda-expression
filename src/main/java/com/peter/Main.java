389ab04e8c73681f48ebfa3f542c0a5fa3be3a97
PeterAmisy <amisy.peter@gmail.com>
2022-12-06 09:51:25 +0100

    Lambda-expression set second commit

--- src/main/java/com/peter/Main.java
+++ src/main/java/com/peter/Main.java
@@ -1,6 +1,7 @@
 package com.peter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,10 +13,19 @@ public class Main {
             }
         }
     }
+    public static void printPeopleWithRange(
+            List<People> people , int low, int high
+    ){
+        List<People> collect = people.stream()
+                .filter(p -> p.getAge() >= low && p.getAge() <= high)
+                .collect(Collectors.toList());
+        System.out.println(collect);
+    }
     public static void main(String... args) {
 
         List<People> peopleList =People.defaultList();
         printPeopleOlderThan(peopleList,25);
+        printPeopleWithRange(peopleList,25,34);
 
     }
 }
