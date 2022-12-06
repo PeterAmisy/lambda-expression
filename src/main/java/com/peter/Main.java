c6e5af42fe7c3f0e520b05cbbd62957fcc3fa144
PeterAmisy <amisy.peter@gmail.com>
2022-12-06 09:44:05 +0100

    Lambda-expression set first commit

--- src/main/java/com/peter/Main.java
+++ src/main/java/com/peter/Main.java
@@ -3,10 +3,19 @@ package com.peter;
 import java.util.List;
 
 public class Main {
+
+    public static void printPeopleOlderThan(List<People>people,
+                                            int age){
+        for (People p: people) {
+            if(p.getAge() >= age){
+                p.printPeople();
+            }
+        }
+    }
     public static void main(String... args) {
 
         List<People> peopleList =People.defaultList();
-
+        printPeopleOlderThan(peopleList,25);
 
     }
 }
